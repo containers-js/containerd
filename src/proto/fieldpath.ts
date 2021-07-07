d254ca336afeab7d840e0c8a6a4e6ae31cf04018
Jacob Gillespie <jacobwgillespie@gmail.com>
2021-07-07 23:54:48 +0100

    Remove unused code

--- src/proto/fieldpath.ts
+++ src/proto/fieldpath.ts
@@ -1,9 +1,5 @@
 import type {MessageTypeDefinition} from '@grpc/proto-loader'
 
-type SubtypeConstructor<Constructor extends new (...args: any) => any, Subtype> = {
-  new (...args: ConstructorParameters<Constructor>): Subtype
-}
-
 export interface ProtoGrpcType {
   containerd: {
     plugin: {}
